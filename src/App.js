1031abae70767d41a27f8114c5562bdb9da7cb30
Prerna Rajput <prerna.rajput@avizva.com>
2025-05-24 23:02:49 +0530

    fix: added css changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./App.css";
 import "h8k-components";
@@ -6,12 +7,17 @@ import Home from "./components/Home";
 const title = "Blog Post";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h8k-navbar header={title}></h8k-navbar>
-      <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav class="navbar navbar-light bg-light">
+                <div class="navbar" href="#">
+                    <img src="/logo.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+                    {title}
+                </div>
+            </nav>
+            <Home />
+        </div>
+    );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import { applyPolyfills, defineCustomElements } from "h8k-components/loader";
 import registerServiceWorker from "./registerServiceWorker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
